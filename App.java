9f15a2340a82d7a8cad35ac76ebd2b02b5990bdd
Gru935 <j.sostruznik@edu.pucrs.br>
2025-03-18 15:43:26 -0300

    Ajeitando o método encontraFatoresPrimos: agora ele consegue utilizar um mesmo fator novamente caso a divisão seja possível

--- App.java
+++ App.java
@@ -1,52 +1,61 @@
-// 1 - 17576 =              0 milissegundos
-// 1 - 105456 =             2 milissegundos
-// 1 - 100.391.791.500 =    0,004 segundos
-
 import java.util.ArrayList;
 
 public class App {
 
     public static void main(String[] args) {
-        // double start = System.currentTimeMillis();
-
-        // int count = 1;
-        // while (count < (1003917915 * 100)) {
-        // count++;
-        // }
-
-        // double elapsed = System.currentTimeMillis() - start;
-        // System.out.println((elapsed / 1000));
-
-        for (int i = 0; i < encontraFatoresPrimos().size(); i++) {
-            System.out.println(encontraFatoresPrimos().get(i));
+        // System.out.println(contaTempo(1003917915 * 100));
+        System.out.print("(");
+        for (int i = 0; i < encontraFatoresPrimos(1700).size(); i++) {
+            System.out.print(encontraFatoresPrimos(1700).get(i) + ",");
         }
+        System.out.println(")");
     }
 
-    public static ArrayList<Integer> encontraFatoresPrimos() {
+    public static double contaTempo(int num) {
+        double start = System.currentTimeMillis();
+        int count = 0;
+
+        while (count < num) {
+            System.out.println(count);
+            count++;
+        }
+
+        double elapsed = System.currentTimeMillis() - start;
+        return (elapsed / 1000);
+    }
+
+    public static ArrayList<Integer> encontraFatoresPrimos(int numComposto) {
         ArrayList<Integer> fatoresPrimos = new ArrayList<>();
-        int numComposto = 66;
         int fator = 2;
         int divisores = 0;
         int divisor = 1;
 
         while (fator <= numComposto) {
-            // Descobre se eh primo
+            // Descobre a quantidade de divisores do fator
             while (divisor <= fator) {
                 if (fator % divisor == 0) {
                     divisores++;
                 }
                 divisor++;
             }
-            // Se for primo, descobre se eh divisor do numero composto
+            // Se o fator for primo, descobre se eh divisor do numero composto
             if (divisores == 2) {
+                // Se o fator for divisor, adiciona na lista e divide o numero composto
                 if (numComposto % fator == 0) {
                     fatoresPrimos.add(fator);
                     numComposto = numComposto / fator;
+                    // Se nao for divisor, vai para o proximo numero
+                } else {
+                    fator++;
+                    divisores = 0;
+                    divisor = 1;
                 }
+                // Se nao for primo, vai para o proximo numero
+            } else {
+                fator++;
+                divisores = 0;
+                divisor = 1;
             }
-            fator++;
-            divisores = 0;
-            divisor = 1;
         }
         return fatoresPrimos;
     }
